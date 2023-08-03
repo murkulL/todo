87bf42e23a2b64d936ef3dfe417af4509445d20c
murkulL <olegov23445@gamil.com>
2023-08-03 18:50:55 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -74,9 +74,3 @@ function App() {
 export default App;
 
 
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/murkulL/todo.git
-git push -u origin main
